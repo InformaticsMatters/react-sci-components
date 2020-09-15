4749d26b5c21e15ae524d2e83e4c22cd02e85796
Oliver Dudgeon <oliverdudgeon@gmail.com>
2020-09-15 11:26:41 +0100

    Attempt to authenticate without redirect & cache tokens

--- src/index.tsx
+++ src/index.tsx
@@ -21,6 +21,12 @@ const render = () => {
 
 render();
 
+// Hot reloading for testing of css/display changes
+// May cause weirdness
+if (process.env.NODE_ENV === 'development' && module.hot) {
+  module.hot.accept('./App', render);
+}
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
