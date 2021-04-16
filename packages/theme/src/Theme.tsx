c32efa3482db6b40bcb7a41cae63364e664b99a9
Oliver Dudgeon <22367286+OliverDudgeon@users.noreply.github.com>
2021-04-16 11:37:12 +0100

    feat(theme): :sparkles: Add more overrides and add a theme component that doesn't require styled components

--- packages/theme/src/Theme.tsx
+++ packages/theme/src/Theme.tsx
@@ -7,58 +7,94 @@ import {
   StylesProvider,
   ThemeProvider as MuiThemeProvider,
 } from '@material-ui/core/styles';
-
-const palette = {
-  common: {
-    black: '#000',
-    white: 'rgba(236, 240, 241, 1)',
-  },
-  background: {
-    paper: '#fff',
-    default: '#fafafa',
-  },
-  primary: {
-    light: 'rgba(255, 125, 102, 1)',
-    main: 'rgba(229, 74, 59, 1)',
-    dark: 'rgba(172, 7, 19, 1)',
-    contrastText: '#fff',
-  },
-  secondary: {
-    light: 'rgba(225, 255, 255, 1)',
-    main: 'rgba(175, 207, 207, 1)',
-    dark: 'rgba(127, 158, 158, 1)',
-    contrastText: '#fff',
-  },
-  error: {
-    light: '#e57373',
-    main: '#f44336',
-    dark: '#d32f2f',
-    contrastText: '#fff',
-  },
-  text: {
-    primary: 'rgba(0, 0, 0, 0.87)',
-    secondary: 'rgba(0, 0, 0, 0.54)',
-    disabled: 'rgba(0, 0, 0, 0.38)',
-    hint: 'rgba(0, 0, 0, 0.38)',
-  },
-};
+import createBreakpoints from '@material-ui/core/styles/createBreakpoints';
+import shadows from '@material-ui/core/styles/shadows';
 
 const family = ['"Open Sans"', 'Verdana', 'Geneva', 'Tahoma', 'sans-serif'].join(', ');
 
-const overrides = {
-  MuiInput: {
-    input: {
-      fontSize: '0.5rem',
-      padding: '6 4.5 6 4.5',
+const breakpoints = createBreakpoints({});
+
+export const theme = createMuiTheme({
+  palette: {
+    common: {
+      black: '#000',
+      white: 'rgba(236, 240, 241, 1)',
+    },
+    background: {
+      paper: '#fff',
+      default: '#fafafa',
+    },
+    primary: {
+      light: 'rgba(255, 125, 102, 1)',
+      main: 'rgba(229, 74, 59, 1)',
+      dark: 'rgba(172, 7, 19, 1)',
+      contrastText: '#fff',
+    },
+    secondary: {
+      light: 'rgba(225, 255, 255, 1)',
+      main: 'rgba(175, 207, 207, 1)',
+      dark: 'rgba(127, 158, 158, 1)',
+      contrastText: '#fff',
+    },
+    error: {
+      light: '#e57373',
+      main: '#f44336',
+      dark: '#d32f2f',
+      contrastText: '#fff',
+    },
+    text: {
+      primary: 'rgba(0, 0, 0, 0.87)',
+      secondary: 'rgba(0, 0, 0, 0.54)',
+      disabled: 'rgba(0, 0, 0, 0.38)',
+      hint: 'rgba(0, 0, 0, 0.38)',
     },
   },
-};
-
-const typography = {
-  fontFamily: family,
-};
-
-export const theme = createMuiTheme({ palette, overrides, typography });
+  overrides: {
+    // MuiInput: {
+    //   input: {
+    //     fontSize: '0.5rem',
+    //     padding: '6 4.5 6 4.5',
+    //   },
+    // },
+    MuiAppBar: {
+      root: {
+        boxShadow: shadows[1],
+      },
+    },
+    MuiToolbar: {
+      root: {
+        minHeight: 60,
+      },
+      regular: {
+        [breakpoints.up('xs')]: {
+          minHeight: 60,
+        },
+      },
+    },
+    MuiTab: {
+      root: {
+        textTransform: 'none',
+        [breakpoints.up('xs')]: {
+          minWidth: 120,
+        },
+      },
+      textColorInherit: {
+        opacity: 1,
+      },
+    },
+    MuiTabs: {
+      root: {
+        minHeight: 60,
+      },
+      flexContainer: {
+        height: '100%',
+      },
+    },
+  },
+  typography: {
+    fontFamily: family,
+  },
+});
 
 export const MuiTheme: React.FC = ({ children }) => {
   return (
