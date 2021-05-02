a1be071283f9f3c16481a88891357a2988f55706
Oliver Dudgeon <22367286+OliverDudgeon@users.noreply.github.com>
2021-05-02 11:47:34 +0100

    fix(orval): :alien: Update operation translations due to API change

--- packages/data-manager-client/input-transformer.js
+++ packages/data-manager-client/input-transformer.js
@@ -7,6 +7,7 @@ const translations = {
   'app.api_dataset.get_dataset': 'downloadDataset',
   'app.api_dataset.delete_editor': 'removeEditorFromDataset',
   'app.api_dataset.put_editor': 'addEditorToDataset',
+  'app.api_file.post': 'attachFile',
   'app.api_file.delete': 'deleteFile',
   'app.api_file.get': 'getFile',
   'app.api_instance.get': 'getInstances',
@@ -17,7 +18,6 @@ const translations = {
   'app.api_project.post': 'addNewProject',
   'app.api_project.delete': 'deleteProject',
   'app.api_project.get_project': 'getProject',
-  'app.api_project.put_dataset': 'addDatasetToProject',
   'app.api_project.delete_editor': 'removeEditorFromProject',
   'app.api_project.put_editor': 'addEditorToProject',
   'app.api_task.get': 'getTasks',
@@ -33,7 +33,7 @@ module.exports = (obj) => {
       if (translations[operationId]) {
         defn.operationId = translations[operationId];
       } else {
-        console.log(operationId);
+        console.warn(`Missing OperationId Rename: ${operationId}`);
       }
     }
   }
