969f6ed7153aec1d8356c93505e5b6ff738afdde
Oliver Dudgeon <22367286+OliverDudgeon@users.noreply.github.com>
2021-06-17 13:20:20 +0100

    chore(release): Force a new version

--- packages/data-manager-client/orval.config.js
+++ packages/data-manager-client/orval.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   'data-manager-api': {
     input: {
-      // validation: true,
       target: 'https://squonk.informaticsmatters.org/data-manager-api/openapi.json',
       override: {
         transformer: './src/input-transformer.js',
@@ -11,7 +10,6 @@ module.exports = {
       mode: 'tags',
       target: './src/orval/data-manager-api.ts',
       schemas: './src/orval/model',
-      // mock: true,
       client: 'react-query',
       override: {
         mutator: {
